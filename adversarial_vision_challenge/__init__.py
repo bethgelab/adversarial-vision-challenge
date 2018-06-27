486d69b6c24e3bee23c043155ef13d513caac322
S.P. Mohanty <spmohanty91@gmail.com>
2018-06-27 02:50:32 +0200

    Expose attack_complete function

--- adversarial_vision_challenge/__init__.py
+++ adversarial_vision_challenge/__init__.py
@@ -10,3 +10,4 @@ from .utils import load_model  # noqa: F401
 from .utils import read_images  # noqa: F401
 from .utils import store_adversarial  # noqa: F401
 from .utils import get_test_data  # noqa: F401
+from .utils import attack_complete # noqa: F401
